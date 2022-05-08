3fc708d3d19ca41f3b74d6eedd9e46b3a02b3ef3
ash2262 <ash2262@gmail.com>
2022-05-09 00:33:22 +0530

    Changes on 9 May 2022
    Changes on 9 May 2022

--- CompareSortedArr/src/CompareSortedArr.java
+++ CompareSortedArr/src/CompareSortedArr.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class CompareSortedArr {
@@ -5,34 +6,47 @@ public class CompareSortedArr {
         //int[] A={1, 7, 11, 8, 11, 7, 1};
         //int B[][]={{0, 2, 4, 6}};
         //expected ans=1
+        int[] arrA={1, 3, 2};
+                ArrayList<Integer> A=new ArrayList<>();
+                for (int j = 0; j < arrA.length; j++) {
+                    A.add(arrA[j]);
+                }
 
-        int[] A={1, 3, 2};
+
         int B[][]={{0, 1, 1, 2}};
         ////expected ans=0
-        
-        int[] ans= solve(A,B);;
+        ArrayList<Integer> ans=new ArrayList<>();
+         ans= solve(A,B);;
         for (int i = 0; i <ans.length ; i++) {
             System.out.println(ans[i]);
         }
     }
-    public static int[] solve(int[] A, int[][] B) {
-        HashSet<Integer> hs=new HashSet<Integer>();
-        int start1= B[0][0];
-        int end1=B[0][1];
-        int[] res = new int[B.length];
-        res[0]=0;
-        int start2=B[0][2];
-        int end2=B[0][3];
-
-        for (int i = start1; i <= end1; i++) {
-        hs.add(A[i]);
+    public static ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B) {
+        int n = A.size();
+        Random rd = new Random();
+        HashMap < Integer, Long > hm = new HashMap < > ();
+        HashSet < Long > used = new HashSet < > ();
+        long psum[] = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            if (!hm.containsKey(A.get(i))) {
+                long hash = rd.nextLong();
+                while (used.contains(hash)) {
+                    hash = rd.nextLong();
+                }
+                hm.put(A.get(i), hash);
+            }
+            psum[i + 1] = psum[i] + hm.get(A.get(i));
         }
-
-        for (int i = start2; i <= end2; i++) {
-            if(!hs.contains(A[i]))
-                return res;
+        ArrayList < Integer > ans = new ArrayList < Integer > ();
+        for (ArrayList < Integer > tmp: B) {
+            long val1 = psum[tmp.get(1) + 1] - psum[tmp.get(0)];
+            long val2 = psum[tmp.get(3) + 1] - psum[tmp.get(2)];
+            if (val1 == val2) {
+                ans.add(1);
+            } else {
+                ans.add(0);
+            }
         }
-        res[0]=1;
-        return  res;
+        return ans;
     }
 }

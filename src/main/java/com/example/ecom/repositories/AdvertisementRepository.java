a3222c9e2dac0f4f00d52864d500fcabfa80be6d
scaler <>
2025-04-26 16:52:51 +0000

    docs: SUBMISSION_COMMIT

--- src/main/java/com/example/ecom/repositories/AdvertisementRepository.java
+++ src/main/java/com/example/ecom/repositories/AdvertisementRepository.java
@@ -3,10 +3,12 @@ package com.example.ecom.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.ecom.models.Advertisement;
 import com.example.ecom.models.Preference;
 
+@Repository
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Integer>{
 Optional<Advertisement> findByPreference(Preference preference);
 }
--- src/main/java/com/example/ecom/repositories/PreferencesRepository.java
+++ src/main/java/com/example/ecom/repositories/PreferencesRepository.java
@@ -1,8 +1,10 @@
 package com.example.ecom.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.ecom.models.Preference;
 
+@Repository
 public interface PreferencesRepository extends JpaRepository<Preference, Integer>{
 }

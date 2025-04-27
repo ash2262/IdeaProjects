2852947695dc8d0707928b26e27b44e0bbd87b30
scaler <>
2025-04-27 17:28:00 +0000

    docs: SUBMISSION_COMMIT

--- src/main/java/com/example/ecom/models/BaseModel.java
+++ src/main/java/com/example/ecom/models/BaseModel.java
@@ -1,6 +1,8 @@
 package com.example.ecom.models;
 
 
+import java.util.Date;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,12 +11,16 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
+
 @Getter
 @Setter
+@Data
 @MappedSuperclass
 public abstract class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private Date createdAt;
+
+
 }

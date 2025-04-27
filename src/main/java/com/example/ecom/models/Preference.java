f17944550251c4a0fc8e7d34dbf06c7b26bb8a25
scaler <>
2025-04-27 17:00:45 +0000

    docs: SUBMISSION_COMMIT

--- src/main/java/com/example/ecom/models/Preference.java
+++ src/main/java/com/example/ecom/models/Preference.java
@@ -4,12 +4,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.util.Date;
+
 import jakarta.persistence.Entity;
 
-
+@Entity
 @Data
 @EqualsAndHashCode(callSuper=true)
-@Entity
 public class Preference extends BaseModel{
     private String category;
     private String description;

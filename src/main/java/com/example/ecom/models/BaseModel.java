96e180ffe5e0707aba9d085d53f9c80aad1db7b7
scaler <>
2025-04-27 17:24:24 +0000

    docs: SUBMISSION_COMMIT

--- src/main/java/com/example/ecom/models/BaseModel.java
+++ src/main/java/com/example/ecom/models/BaseModel.java
@@ -6,8 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BaseModel {
     @Id
